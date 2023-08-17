adb6371d26aa2850ad1e28c8808e8ae404e051e9
Shivachauhan17 <shivanbd2019@gmail.com>
2023-08-17 12:15:45 +0530

    wrote server and writing controllers,routes

---
+++ routes/main.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const router=express.Router()
+const authController=require('../controllers/auth.js')
+const homeController=require('../controllers/home')
+const postsController=require('controllers/posts')
+
+router.get("/", homeController.getIndex);
+router.get("/profile", ensureAuth, postsController.getProfile);
+router.get("/feed",postsController.getFeed)
+router.get("/login",authController.getLogin)
+router.post("/login",authController.podtLogin)
+router.get("/logout",authController.getLogout)
+router.get("/signup",authController.getSignup)
+router.post("/signup",authController.postSignup)
+
+module.exports=router
---
+++ server.js
@@ -0,0 +1,58 @@
+const express=require('express')
+const app=express()
+const mongoose=require('mongoose')
+const {urlencoded}=require('body-parser')
+const passport=require('passport')
+const session=require('express-session')
+const MongoStore=require('connect-mongo')
+const flash=require('express-flash')
+const logger=require('morgan')
+
+
+const connectDB=require('./config/database')
+require("dotenv").config({ path: "./config/.env" });
+
+const mainRoutes=require('./routes/main')
+const postRoutes=require('./routes/post')
+
+require('./config/passport')(passport)
+
+connectDB()
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(logger('dev'))
+
+let store = new MongoStore({
+    mongoUrl: process.env.DB_STRING,
+    collection: "sessions"
+  });
+
+
+app.use(
+session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    // so we are gonna have our session info in our mongo database
+    // store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store:store,
+})
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+
+//Setup Routes For Which The Server Is Listening
+app.use("/", mainRoutes);
+// app.use("/post", postRoutes);
+
+//Server Running
+app.listen(process.env.PORT, () => {
+  console.log("Server is running, you better catch it!");
+});
+
+
